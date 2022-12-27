5ab86ff674a00dcb342addbafeeef0454378a75b
Pandeeswaran <49590510+pandeeswaran7373@users.noreply.github.com>
2022-12-27 17:54:31 +0530

    Update PythonBasics.py

--- PythonBasics.py
+++ PythonBasics.py
@@ -1,10 +1,10 @@
 # python basics 
 #input function
-#print("hello " + input("What is Your Name?"))
+print("hello " + input("What is Your Name?"))
 
 # len function in python using input 
-#print(len(input("")))
-#print(len(input('What is your name? ')))
+print(len(input("")))
+print(len(input('What is your name? ')))
 
 #varibles
 name=input("What is Your name?")
