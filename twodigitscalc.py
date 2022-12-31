34834e8f9013a643cb6bc5df785bc74aad3af1dc
Pandeeswaran <49590510+pandeeswaran7373@users.noreply.github.com>
2022-12-31 10:23:37 +0530

    Create twodigitscalc.py
    using str to int convertion method

---
+++ twodigitscalc.py
@@ -0,0 +1,12 @@
+#calculate input numbers
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+num1=int(two_digit_number[0])
+num2=int(two_digit_number[1])
+#calculate the input two digits
+calc=num1+num2
+print(calc)
